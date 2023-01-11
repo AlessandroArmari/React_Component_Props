593d506e8d2a053f41a9b2b330b91575f94382e1
hobisognodiunarma <alessandroarmari22@gmail.com>
2023-01-11 19:10:26 +0100

    First Part

---
+++ ReactComponentProps/src/App.jsx
@@ -0,0 +1,33 @@
+//We are creating a Component
+//wich behaves as a <div> or <span>...
+//...it creates a <h1> with stuff inside, indeed
+
+//It's important not to forget the {} for the paramaters
+const Component = ({ firstParam, secondParam }) => {
+  return (
+    <>
+      <div>I am a "custom_made" component</div>
+      <div>
+        wich also has 2 parameters: "{firstParam}" and "{secondParam}"
+      </div>
+    </>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <h1>Hello! (I don't belong to the component)</h1>
+      <br />
+      <br />
+      <Component firstParam={44} secondParam={55} />
+
+      <br />
+      <br />
+
+      <Component firstParam={"Parameter Changed"} secondParam={"Again"} />
+    </>
+  );
+}
+
+export default App;
